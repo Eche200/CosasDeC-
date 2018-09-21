5d2129045052b41cd2b43339f357f7ed548d609e
Eche200 <eecheconea@gmail.com>
2018-09-21 15:24:27 -0300

    clases , objetos , get and set

---
+++ EtapaUno/Clases/Escuela.cs
@@ -0,0 +1,59 @@
+class Escuela
+{
+
+
+    public string nombre;
+    public int anoDeCreacion;
+    public string paisDeLaEscuela;
+
+
+    public Escuela(){
+
+    }
+
+
+    public string Nombre
+    {
+        get
+        {
+            return this.nombre;
+        }
+        set
+        {
+            this.nombre = value;
+        }
+    }
+    public int AnoDeCreacion
+    {
+        get
+        {
+            return this.anoDeCreacion;
+        }
+        set
+        {
+            this.anoDeCreacion = value;
+        }
+    }
+    public string PaisDeLaEscuela
+    {
+        get
+        {
+            return this.paisDeLaEscuela;
+        }
+        set
+        {
+            this.paisDeLaEscuela = value;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ EtapaUno/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EtapaUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var instanciaDeEscuela = new Escuela();
+
+
+           instanciaDeEscuela.Nombre = "San Cristobal";
+           instanciaDeEscuela.AnoDeCreacion = 1950;
+           instanciaDeEscuela.PaisDeLaEscuela = "Argentina";
+
+           Console.WriteLine("Nombre del colegio: " + instanciaDeEscuela.Nombre + "\nAño de creacion de la escuela: " + 
+           instanciaDeEscuela.AnoDeCreacion + "\nPais de origen de la escuela: " + instanciaDeEscuela.PaisDeLaEscuela);
+
+
+        }
+    }
+}
